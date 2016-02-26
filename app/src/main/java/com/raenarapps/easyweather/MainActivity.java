5bccb5affd6d7c94b4857fa9d9ff65f71c467a39
Raenar4k <RaenarDev@gmail.com>
2016-02-26 19:42:41 +0200

    added current geolocation auto selection on the first run

--- app/src/main/java/com/raenarapps/easyweather/MainActivity.java
+++ app/src/main/java/com/raenarapps/easyweather/MainActivity.java
@@ -3,24 +3,40 @@ package com.raenarapps.easyweather;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements ForecastFragment.Callback {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements ForecastFragment.Callback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     public static final int INTERNET_REQUEST_CODE = 1;
+    private static final int COARSE_LOCATION_REQUEST_CODE = 2;
     private static final String TAG = MainActivity.class.getSimpleName();
     public static final String DETAIL_FRAGMENT_TAG = DetailFragment.class.getSimpleName();
+
     private String locationSetting;
     boolean isTwoPane;
+    private GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,22 +63,47 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
         locationSetting = Utility.getPreferredLocation(this);
 
         if (locationSetting.equals(getString(R.string.pref_location_default))) {
-            showLocationSetup();
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
         }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ForecastFragment forecastFragment = (ForecastFragment) getSupportFragmentManager()
-                        .findFragmentById(R.id.fragment_forecast);
-                forecastFragment.updateLocation();
+                updateWeatherData();
             }
         });
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.INTERNET)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, INTERNET_REQUEST_CODE);
+        }
+    }
+
+    private void updateWeatherData() {
+        ForecastFragment forecastFragment = (ForecastFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragment_forecast);
+        forecastFragment.updateLocation();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (googleApiClient != null && Utility.getPreferredLocation(this).equals(getString(R.string.pref_location_default))) {
+            googleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (googleApiClient != null) {
+            googleApiClient.disconnect();
         }
     }
 
@@ -78,6 +119,13 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
                     finish();
                 }
                 break;
+            case COARSE_LOCATION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getLastKnownLocation();
+                } else {
+                    startManualLocationSetup();
+                }
+                break;
         }
     }
 
@@ -87,9 +135,9 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
         if (Utility.getPreferredLocation(this) != locationSetting) {
             ForecastFragment forecastFragment = (ForecastFragment) getSupportFragmentManager()
                     .findFragmentById(R.id.fragment_forecast);
-            locationSetting = Utility.getPreferredLocation(this);
             forecastFragment.updateLocation();
             if (isTwoPane) {
+                locationSetting = Utility.getPreferredLocation(this);
                 DetailFragment detailFragment = (DetailFragment) getSupportFragmentManager()
                         .findFragmentByTag(DETAIL_FRAGMENT_TAG);
                 detailFragment.updateLocation(locationSetting);
@@ -97,7 +145,7 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
         }
     }
 
-    private void showLocationSetup() {
+    private void startManualLocationSetup() {
         Intent intent = new Intent(this, SettingsActivity.class);
         intent.putExtra(Intent.EXTRA_TEXT, "firstLaunch");
         startActivity(intent);
@@ -134,5 +182,55 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
             intent.setData(uri);
             startActivity(intent);
         }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        getLastKnownLocation();
+    }
+
+    private void getLastKnownLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, COARSE_LOCATION_REQUEST_CODE);
+            return;
+        }
+        Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                googleApiClient);
+        if (lastLocation != null) {
+            double lat = lastLocation.getLatitude();
+            double lon = lastLocation.getLongitude();
+//            Log.d(TAG, "lastLocation lat,lon =" + lat + "," + lon);
+            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+            List<Address> addresses = null;
+            try {
+                addresses = geocoder.getFromLocation(lat, lon, 1);
+            } catch (IOException e) {
+                startManualLocationSetup();
+                e.printStackTrace();
+            }
+            if (addresses != null) {
+                String cityName = addresses.get(0).getLocality();
+                String adminArea = addresses.get(0).getAdminArea();
+                String countryName = addresses.get(0).getCountryName();
+                String locationString = cityName + ", " + adminArea + ", " + countryName;
+//                Log.d(TAG, "lastLocation =" + locationString);
+                PreferenceManager.getDefaultSharedPreferences(this).edit()
+                        .putString(getString(R.string.pref_location_key), locationString)
+                        .apply();
+                updateWeatherData();
+            }
+        } else {
+            startManualLocationSetup();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "GoogleApiClient - onConnectionSuspended");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "GoogleApiClient - onConnectionFailed");
     }
 }
